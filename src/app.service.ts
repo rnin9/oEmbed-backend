c1a2ebe166c06d1054b538e39bb77573eaf148b8
=rnin9 <=rkdalswn1209>
2022-01-14 19:54:06 +0900

    add comment on Service

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,68 +7,52 @@ import { urlDto } from './dto/url.dto';
 @Injectable()
 export class AppService {
   constructor(private httpService: HttpService) {}
-  async getData(url: urlDto): Promise<AxiosResponse> {
-    const urlSchema = new URL(url.url);
-    console.log(urlSchema);
-    //get hostname to make branch
-    if (urlSchema.hostname === 'www.youtube.com') {
-      const special = 'https://www.youtube.com/oembed?url=';
-      const query = await this.makeUrl(url);
-      const api = special + query;
-      const resp = this.httpService.get(api).pipe(map((res) => res.data)); //observable
-      const data = await lastValueFrom(resp); // lastValueFrom (await)
-      console.log(api);
-      return data;
-    } else if (urlSchema.hostname === 'www.instagram.com') {
-      const special = 'https://api.instagram.com/oembed/?url=';
-      const query = await this.makeUrl(url);
-      const api = special + query;
-      const resp = this.httpService.get(api).pipe(map((res) => res.data)); //observable
-      const data = await lastValueFrom(resp); // lastValueFrom (await)
-      console.log(api);
-
-      return data;
-    } else if (urlSchema.hostname === 'twitter.com') {
-      const special = 'https://publish.twitter.com/oembed?url=';
-      const query = await this.makeUrl(url);
-      const api = special + query;
-      const resp = this.httpService.get(api).pipe(map((res) => res.data)); //observable
-      const data = await lastValueFrom(resp); // lastValueFrom (await)
-      console.log(api);
-
-      return data;
-    } else if (urlSchema.hostname === 'vimeo.com') {
-      const special = 'https://vimeo.com/api/oembed.json?url=';
-      const query = await this.makeUrl(url);
-      const api = special + query;
-      const resp = this.httpService.get(api).pipe(map((res) => res.data)); //observable
-      const data = await lastValueFrom(resp); // lastValueFrom (await)
-      console.log(api);
-
-      return data;
-    } else if (
-      urlSchema.hostname === 'www.flickr.com' ||
-      urlSchema.hostname === 'flickr.com'
-    ) {
-      const special = 'http://www.flickr.com/services/oembed/?url=';
-      const query = await this.makeUrl(url);
-      const api = special + query;
-      console.log(api);
-      const resp = this.httpService.get(api).pipe(map((res) => res.data)); //observable
-      const data = await lastValueFrom(resp); // lastValueFrom (await)
-
-      return data;
-    }
+  //Getting oembed data Main Logic
+  async getData(urlData: urlDto): Promise<AxiosResponse> {
+    const urlSchema = new URL(urlData.url);
+    //make url process, use two async function parallelly with promise.all()
+    const api = await Promise.all([
+      //selecting api by url's hostname
+      this.selectApi(urlSchema.hostname).catch((err) => {
+        return err;
+      }),
+      //make query url with parameters made by urlDto elements
+      this.makeQuery(urlData).catch((err) => {
+        return err;
+      }),
+    ]).then((subUrls) => subUrls.join('+'));
+    //using Rxjs to get Data when api URL Ready.
+    const resp = this.httpService.get(api).pipe(map((res) => res.data));
+    // subscribe and await until operation finished, and convert to promise
+    const data = await lastValueFrom(resp);
+    return data;
   }
-
-  async makeUrl(url: urlDto): Promise<string> {
-    //map으로 변경?
+  //Query url making Logic
+  async makeQuery(url: urlDto): Promise<string> {
     const urlSchema = new URL(url.url);
     const params = new URLSearchParams(urlSchema.search);
+    //append all parameters which is in urlDto element to url
     Object.keys(url).map((query) => {
       params.append(query, url[query].toString());
     });
-    console.log(urlSchema.toString() + params.toString());
     return urlSchema.toString() + '&' + params.toString();
+  }
+
+  //select api by hostname
+  async selectApi(hostname: string): Promise<string> {
+    //selecting api logic
+    if (hostname === 'www.youtube.com') {
+      return 'https://www.youtube.com/oembed?url=';
+    } else if (hostname === 'www.instagram.com') {
+      return 'https://api.instagram.com/oembed/?url=';
+    } else if (hostname === 'twitter.com') {
+      return 'https://publish.twitter.com/oembed?url=';
+    } else if (hostname === 'vimeo.com') {
+      return 'https://vimeo.com/api/oembed.json?url=';
+    } else if (hostname === 'www.flickr.com' || hostname === 'flickr.com') {
+      return 'http://www.flickr.com/services/oembed/?url=';
+    } else {
+      return undefined;
+    }
   }
 }
