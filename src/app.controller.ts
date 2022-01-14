df3e0b93543953ccfc2611f2d067d5217bbfe334
=rnin9 <=rkdalswn1209>
2022-01-14 09:48:15 +0900

    Make axios connection and check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,9 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getData(@Query() url: string) {
+    try {
+      this.appService.getData(url);
+    } catch (err) {}
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private httpService: HttpService) {}
+  async getData(url: string): Promise<Observable<AxiosResponse<any, any>>> {
+    return this.httpService.get(url);
   }
 }
